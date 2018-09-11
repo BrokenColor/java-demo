781784ebb632933cfb1a409ee4c058d9add2fdb3
BrokenColor <huajianzhiyue@sina.com>
2018-09-11 16:33:54 +0800

    多任务同时处理任务-ReentrantLock(阻塞)

--- src/main/java/concurrenttest/semaphoredemo/runtest/RunTest_MoreToOne.java
+++ src/main/java/concurrenttest/semaphoredemo/runtest/RunTest_MoreToOne.java
@@ -1,12 +1,10 @@
 package concurrenttest.semaphoredemo.runtest;
 
 import concurrenttest.semaphoredemo.pojo.ThreadA;
-import concurrenttest.semaphoredemo.pojo.ThreadB;
-import concurrenttest.semaphoredemo.pojo.ThreadC;
 import concurrenttest.semaphoredemo.serviceImp.Semaphore_MoreToOne;
 
 /**
- * 描述:
+ * 描述:多任务同时处理任务
  *
  * @author bc
  * @create 2018-09-11 15:31
@@ -16,14 +14,10 @@ public class RunTest_MoreToOne {
         // TODO Auto-generated method stub
         Semaphore_MoreToOne service = new Semaphore_MoreToOne();
 
-        ThreadA a = new ThreadA(service);
-        a.setName("A");
-        ThreadB b = new ThreadB(service);
-        b.setName("B");
-        ThreadC c = new ThreadC(service);
-        c.setName("C");
-        a.start();
-        b.start();
-        c.start();
+        ThreadA[] arr = new ThreadA[10];
+        for (int i = 0; i < arr.length; i++) {
+        	arr[i] = new ThreadA(service);
+        	arr[i].start();
+        }
     }
 }
--- src/main/java/concurrenttest/semaphoredemo/serviceImp/Semaphore_MoreToOne.java
+++ src/main/java/concurrenttest/semaphoredemo/serviceImp/Semaphore_MoreToOne.java
@@ -3,31 +3,33 @@ package concurrenttest.semaphoredemo.serviceImp;
 import concurrenttest.semaphoredemo.service.SemaphoreService;
 
 import java.util.concurrent.Semaphore;
+import java.util.concurrent.locks.ReentrantLock;
 
 /**
- * tryAcquire() 尝试得获取x许可
- *
+ * 多任务同时处理任务
+ *	ReentrantLock(阻塞)
  * @author bc
  * @data 2018年9月9日
  */
 public class Semaphore_MoreToOne implements SemaphoreService {
 
     //类Semaphore的参数代表同一时间内，最多允许多少个线程执行
-//    private Semaphore semaphore = new Semaphore(1);
     private Semaphore semaphore = new Semaphore(3);
+    private ReentrantLock lock = new ReentrantLock();//重入锁
 
     public void testMethod() {
         try {
             semaphore.acquire();
-            System.out.println("Name:" + Thread.currentThread().getName() + "首次进入");
-            for (int i = 0; i < Integer.MAX_VALUE / 50; i++) {
-                String newString = new String();
-                Math.random();
-//                System.out.println(Thread.currentThread().getName()+"正在运行中...");
+            System.out.println("Name:" + Thread.currentThread().getName() + "-准备");
+            lock.lock();
+            System.out.println("begin: " + System.currentTimeMillis());
+            for (int i = 0; i < 5; i++) {
+                System.out.println(Thread.currentThread().getName()+"打印"+(i+1));
             }
-//                semaphore.release();
-            semaphore.release(3);
-
+            System.out.println("end: " + System.currentTimeMillis());
+            lock.unlock();
+            semaphore.release();
+            System.out.println("Name:" + Thread.currentThread().getName() + "-结束");
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
