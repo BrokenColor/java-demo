c23455e92db9770c8dc19f5ecda5fbaa16edd979
brokencolor <huajianzhiyue@sina.com>
2018-10-15 15:21:11 +0800

    ThreadPoolExecutor 构造方法test

--- src/main/java/concurrenttest/threadpoolexecutor/runtest/RunTest_Constructor04.java
+++ src/main/java/concurrenttest/threadpoolexecutor/runtest/RunTest_Constructor04.java
@@ -1,6 +1,6 @@
 package concurrenttest.threadpoolexecutor.runtest;
 
-import java.util.concurrent.SynchronousQueue;
+import java.util.concurrent.LinkedBlockingQueue;
 import java.util.concurrent.ThreadPoolExecutor;
 import java.util.concurrent.TimeUnit;
 
@@ -8,12 +8,12 @@ import concurrenttest.threadpoolexecutor.threaddemo.RunnableTest;
 
 /**
  * 描述：ThreadPoolExecutor 构造方法test
- * 队列使用得了SynchronousQueue类  	
- * 线程数>corePoolSize，线程数<=maximumPoolSize
+ * 队列使用得了LinkedBlockingQueue类
+ * 线程数>maximumPoolSize
  * @author BrokenColor
  * @date 2018年10月15日
  */
-public class RunTest_Constructor03 {
+public class RunTest_Constructor04 {
 
 	public static void main(String[] args) throws InterruptedException {
 		
@@ -21,26 +21,25 @@ public class RunTest_Constructor03 {
 		/**
 		 * 	corePoolSize - 即使空闲时仍保留在池中的线程数，除非设置 allowCoreThreadTimeOut
 		 * 	maximumPoolSize - 池中允许的最大线程数
-		 *	keepAliveTime - 当线程数大于核心时，这是多余的空闲线程在终止之前等待新任务的最大时间。等于0时任务完成后立即从队里中删除空闲线程
+		 *	keepAliveTime - 当线程数大于核心时，这是多余的空闲线程在终止之前等待新任务的最大时间。
 		 *	unit - keepAliveTime参数的时间单位
 		 *	workQueue - 在执行任务之前用于保存任务的队列。 该队列将仅保存execute方法提交的Runnable任务。
 		 *------------------------------------------------------------------
-		 *	队列使用得了SynchronousQueue类  
-		 *	线程数（8）>corePoolSize（7）时，线程数（8）<=maximumPoolSize（8）
-		 *	由于运行的线程（8）>corePoolSize数(7)，所以keepAliveTime>5时清除空闲线程
-		 *	如果使用了SynchronousQueue类  则maximumPoolSize参数的作用有效
+		 *	队列使用得了LinkedBlockingQueue类  （沒有大小限制）
+		 *	超过corePoolSize的线程放入LinkedBlockingQueue队列中
+		 *	同一时间只能corePoolSize个线程运行
+		 *	如果使用了LinkedBlockingQueue类，则maximumPoolSize参数的作用将忽略
 		 */
-		//keepAliveTime 等于0时任务完成后立即从队里中删除空闲线程
-		ThreadPoolExecutor executor = new ThreadPoolExecutor(7, 8, 5, TimeUnit.SECONDS, new SynchronousQueue<Runnable>());
-		//线程数>corePoolSize，线程数<=maximumPoolSize
-		for (int i = 0; i < 8; i++) {
+		ThreadPoolExecutor executor = new ThreadPoolExecutor(7, 8, 5, TimeUnit.SECONDS, new LinkedBlockingQueue<Runnable>());
+		//线程数>maximumPoolSize
+		for (int i = 0; i < 9; i++) {
 			executor.execute(runnable);
 		}
-		Thread.sleep(100);
+		Thread.sleep(1000);
 		System.out.println("A.CorePoolSize:"+executor.getCorePoolSize());
 		System.out.println("A.PoolSize:"+executor.getPoolSize());
 		System.out.println("A.Queue.size:"+executor.getQueue().size());
-		Thread.sleep(10000);
+		Thread.sleep(3000);
 		System.out.println("B.CorePoolSize:"+executor.getCorePoolSize());
 		System.out.println("B.PoolSize:"+executor.getPoolSize());
 		System.out.println("B.Queue.size:"+executor.getQueue().size());
--- src/main/java/concurrenttest/threadpoolexecutor/runtest/RunTest_Constructor05.java
+++ src/main/java/concurrenttest/threadpoolexecutor/runtest/RunTest_Constructor05.java
@@ -9,11 +9,11 @@ import concurrenttest.threadpoolexecutor.threaddemo.RunnableTest;
 /**
  * 描述：ThreadPoolExecutor 构造方法test
  * 队列使用得了SynchronousQueue类  	
- * 线程数>corePoolSize，线程数<=maximumPoolSize
+ * 线程数>maximumPoolSize
  * @author BrokenColor
  * @date 2018年10月15日
  */
-public class RunTest_Constructor03 {
+public class RunTest_Constructor05 {
 
 	public static void main(String[] args) throws InterruptedException {
 		
@@ -21,19 +21,17 @@ public class RunTest_Constructor03 {
 		/**
 		 * 	corePoolSize - 即使空闲时仍保留在池中的线程数，除非设置 allowCoreThreadTimeOut
 		 * 	maximumPoolSize - 池中允许的最大线程数
-		 *	keepAliveTime - 当线程数大于核心时，这是多余的空闲线程在终止之前等待新任务的最大时间。等于0时任务完成后立即从队里中删除空闲线程
+		 *	keepAliveTime - 当线程数大于核心时，这是多余的空闲线程在终止之前等待新任务的最大时间。
 		 *	unit - keepAliveTime参数的时间单位
 		 *	workQueue - 在执行任务之前用于保存任务的队列。 该队列将仅保存execute方法提交的Runnable任务。
 		 *------------------------------------------------------------------
 		 *	队列使用得了SynchronousQueue类  
-		 *	线程数（8）>corePoolSize（7）时，线程数（8）<=maximumPoolSize（8）
-		 *	由于运行的线程（8）>corePoolSize数(7)，所以keepAliveTime>5时清除空闲线程
+		 *	线程数（9）>maximumPoolSize（8）,处理maximumPoolSize任务，其他的任务不再处理抛出异常
 		 *	如果使用了SynchronousQueue类  则maximumPoolSize参数的作用有效
 		 */
-		//keepAliveTime 等于0时任务完成后立即从队里中删除空闲线程
 		ThreadPoolExecutor executor = new ThreadPoolExecutor(7, 8, 5, TimeUnit.SECONDS, new SynchronousQueue<Runnable>());
-		//线程数>corePoolSize，线程数<=maximumPoolSize
-		for (int i = 0; i < 8; i++) {
+		//线程数>maximumPoolSize
+		for (int i = 0; i < 9; i++) {
 			executor.execute(runnable);
 		}
 		Thread.sleep(100);
