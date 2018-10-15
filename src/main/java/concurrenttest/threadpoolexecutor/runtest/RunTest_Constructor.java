6c8d3f8e18fadf2bfb0d14579d1117a963f6fbb7
brokencolor <huajianzhiyue@sina.com>
2018-10-15 13:29:19 +0800

    ThreadPoolExecutor 构造方法test

---
+++ src/main/java/concurrenttest/threadpoolexecutor/runtest/RunTest_Constructor.java
@@ -0,0 +1,44 @@
+package concurrenttest.threadpoolexecutor.runtest;
+
+import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
+
+import concurrenttest.threadpoolexecutor.threaddemo.RunnableTest;
+
+/**
+ * 描述：ThreadPoolExecutor 构造方法test
+ * 
+ * @author BrokenColor
+ * @date 2018年10月15日
+ */
+public class RunTest_Constructor {
+
+	public static void main(String[] args) throws InterruptedException {
+		
+		RunnableTest runnable = new RunnableTest();
+		/**
+		 * 	corePoolSize - 即使空闲时仍保留在池中的线程数，除非设置 allowCoreThreadTimeOut
+		 * 	maximumPoolSize - 池中允许的最大线程数
+		 *	keepAliveTime - 当线程数大于核心时，这是多余的空闲线程在终止之前等待新任务的最大时间。
+		 *	unit - keepAliveTime参数的时间单位
+		 *	workQueue - 在执行任务之前用于保存任务的队列。 该队列将仅保存execute方法提交的Runnable任务。
+		 *
+		 */
+		//----LinkedBlockingQueue
+		ThreadPoolExecutor executor = new ThreadPoolExecutor(7, 8, 5, TimeUnit.SECONDS, new LinkedBlockingQueue<>());
+		
+		for (int i = 0; i < 7; i++) {
+			executor.execute(runnable);
+		}
+		Thread.sleep(1000);
+		System.out.println("A.CorePoolSize:"+executor.getCorePoolSize());
+		System.out.println("A.PoolSize:"+executor.getPoolSize());
+		System.out.println("A.Queue.size:"+executor.getQueue().size());
+		Thread.sleep(3000);
+		System.out.println("B.CorePoolSize:"+executor.getCorePoolSize());
+		System.out.println("B.PoolSize:"+executor.getPoolSize());
+		System.out.println("B.Queue.size:"+executor.getQueue().size());
+	}
+
+}
---
+++ src/main/java/concurrenttest/threadpoolexecutor/threaddemo/RunnableTest.java
@@ -0,0 +1,22 @@
+package concurrenttest.threadpoolexecutor.threaddemo;
+
+/**
+ * 描述：线程
+ * 
+ * @author BrokenColor
+ * @date 2018年10月15日
+ */
+public class RunnableTest implements Runnable {
+
+	@Override
+	public void run() {
+		System.out.println(Thread.currentThread().getName()+" run "+System.currentTimeMillis());
+		try {
+			Thread.sleep(1000);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+}
