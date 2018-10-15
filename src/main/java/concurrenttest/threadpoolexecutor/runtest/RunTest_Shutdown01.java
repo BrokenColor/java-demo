bdcc7d51756f7721d5c422071ed3cbb13842c0f7
brokencolor <huajianzhiyue@sina.com>
2018-10-15 19:38:26 +0800

    ThreadPoolExecutor类方法测试

--- src/main/java/concurrenttest/threadpoolexecutor/runtest/RunTest_Shutdown01.java
+++ src/main/java/concurrenttest/threadpoolexecutor/runtest/RunTest_Shutdown01.java
@@ -12,14 +12,19 @@ import concurrenttest.threadpoolexecutor.threaddemo.RunnableTest00;
  * @author BrokenColor
  * @date 2018年10月15日
  */
-public class RunTest_Shutdown {
+public class RunTest_Shutdown01 {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws InterruptedException {
 		RunnableTest00 runnable = new RunnableTest00();
-		ThreadPoolExecutor executor = new ThreadPoolExecutor(7, 10, 0L, TimeUnit.SECONDS, new LinkedBlockingQueue<Runnable>());
-		executor.execute(runnable);
+		ThreadPoolExecutor executor = new ThreadPoolExecutor(2, 99, 0L, TimeUnit.SECONDS, new LinkedBlockingQueue<Runnable>());
+		for (int i = 0; i < 4; i++) {
+			executor.execute(runnable);
+		}
+		Thread.sleep(1000);
 		//启动有序关闭，其中先前提交的任务将被执行，但不会接受任何新任务。
 		executor.shutdown();
+		//因为执行了shutdown方法，当前程序被销毁
+		executor.execute(runnable);
 		System.out.println("main end");
 	}
 
