dbbc0133f09aeb17d589f8fa9a19306de2bcca18
GWL <huajianzhiyue@sina.com>
2018-12-26 21:03:59 +0800

    schedule(Runnable command, long delay, TimeUnit unit)

--- src/main/java/concurrenttest/scheduleexecutorservice/runtest/RunTest_SE_callable.java
+++ src/main/java/concurrenttest/scheduleexecutorservice/runtest/RunTest_SE_callable.java
@@ -15,6 +15,7 @@ import concurrenttest.scheduleexecutorservice.threaddemo.CallableB;
 /**
  * 描述：schedule(Callable<V> callable, long delay, TimeUnit unit)
  * 创建并执行在给定延迟后启用的ScheduledFuture。
+ * 将定时任务与线程池功能结合使用
  * 
  * @author BrokenColor
  * @date 2018年12月25日
@@ -27,11 +28,17 @@ public class RunTest_SE_callable {
 			List<Callable> list = new ArrayList<>();
 			list.add(new CallableA());
 			list.add(new CallableB());
-
+			//单任务
 			ScheduledExecutorService executorService = Executors.newSingleThreadScheduledExecutor();
+			//多任务
+//			ScheduledExecutorService executorService = Executors.newScheduledThreadPool(2);
 			//schedule方法创建具有各种延迟的任务，并返回可用于取消或检查执行的任务对象
-			ScheduledFuture<String> futureA = executorService.schedule(list.get(0), 4L, TimeUnit.SECONDS);
-			ScheduledFuture<String> futureB = executorService.schedule(list.get(1), 4L, TimeUnit.SECONDS);
+			//方法中第二个参数，在多个任务中同时消耗时间
+//			ScheduledFuture<String> futureA = executorService.schedule(list.get(0), 4L, TimeUnit.SECONDS);
+//			ScheduledFuture<String> futureB = executorService.schedule(list.get(1), 4L, TimeUnit.SECONDS);
+			
+			ScheduledFuture<String> futureA = executorService.schedule(list.get(0), 0L, TimeUnit.SECONDS);
+			ScheduledFuture<String> futureB = executorService.schedule(list.get(1), 0L, TimeUnit.SECONDS);
 			
 			System.out.println(" X " + System.currentTimeMillis());
 			System.out.println("A返回值：" + futureA.get());
