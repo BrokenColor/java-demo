e9bb6388372cb36fc4a278ed0d864e60fc7e3202
GWL <huajianzhiyue@sina.com>
2019-01-10 10:49:03 +0800

    try-catch -finally 测试

---
+++ src/main/java/basetest/trycatch.java
@@ -0,0 +1,25 @@
+package basetest;
+
+/**
+ * 描述：try-catch -finally 测试
+ * finally块中的代码一定会执行
+ * 
+ * @author BrokenColor
+ * @date 2019年1月10日
+ */
+public class trycatch {
+
+	public static void main(String[] args) {
+		try {
+			System.out.println("aaa");
+			return ;
+//			System.out.println("bb");
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			System.out.println("finally");
+		}
+
+	}
+
+}
