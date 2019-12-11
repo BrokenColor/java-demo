bef3cff9b6259e5eecfd4358067f7281acccb970
brokencolor <huajianzhiyue@sina.com>
2019-12-11 10:16:07 +0800

    并发CAS AtomicaInteger method test

--- src/main/java/concurrenttest/atomic/AtomicIntegerDefectDemo.java
+++ src/main/java/concurrenttest/atomic/AtomicIntegerDefectDemo.java
@@ -10,11 +10,12 @@ public class AtomicIntegerDefectDemo {
         defectOfABA();
     }
 
-    static void defectOfABA() {
+    private static void defectOfABA() {
         final AtomicInteger atomicInteger = new AtomicInteger(1);
         Thread thread1 = new Thread(() -> {
             final int currentValue = atomicInteger.get();
             System.out.println(Thread.currentThread().getName() + " ------ currentValue=" + currentValue);
+            //为了保证其他线程先执行完
             try {
                 Thread.sleep(300);
             } catch (InterruptedException e) {
@@ -27,6 +28,7 @@ public class AtomicIntegerDefectDemo {
                     + ", compareAndSet Result=" + casResult);
         });
         thread1.start();
+        //为了让 thread1 线程先跑起来
         try {
             Thread.sleep(100);
         } catch (InterruptedException e) {
