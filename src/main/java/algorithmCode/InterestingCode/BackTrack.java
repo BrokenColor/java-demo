0bf51a0ae74638b94951fc085d98442de4ff088a
guwanli <guwanli@ushareit.com>
2021-09-28 20:57:44 +0800

    46 全排列

---
+++ src/main/java/algorithmCode/InterestingCode/BackTrack.java
@@ -0,0 +1,56 @@
+package algorithmCode.InterestingCode;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ * 46 全排列
+ * 回溯算法
+ *
+ * @author guwanli
+ * @date 2021/9/28 8:20 下午
+ */
+public class BackTrack {
+    /**
+     * //输入：nums = [1,2,3]
+     * //输出：[[1,2,3],[1,3,2],[2,1,3],[2,3,1],[3,1,2],[3,2,1]]
+     */
+    static List<List<Integer>> res = new ArrayList<>();
+
+    public static void main(String[] args) {
+        int[] nums = {1, 2, 3};
+        //路径记录
+        LinkedList<Integer> track = new LinkedList<>();
+        backtrack(nums, track);
+        System.out.println(res);
+    }
+
+    /**
+     * 路径：记录在 track 中
+     * 选择列表：nums 中不存在于 track 的那些元素
+     * 结束条件：nums 中的元素全都在 track 中出现
+     *
+     * @param nums
+     * @param track
+     */
+    public static void backtrack(int[] nums, LinkedList<Integer> track) {
+        //结束条件
+        if (nums.length == track.size()) {
+            //添加到结果集
+            res.add(new LinkedList<>(track));
+            return;
+        }
+        //遍历待处理数组
+        for (int num : nums) {
+            //当前循环处理过的跳过
+            if (track.contains(num)) continue;
+            track.add(num);
+            //向下处理
+            backtrack(nums, track);
+            //撤销选择
+            //track用LinkedList是可以使用removeLast进行撤销选择
+            track.removeLast();
+        }
+    }
+}
--- src/main/java/basetest/collections/list/ArrayListTest.java
+++ src/main/java/basetest/collections/list/ArrayListTest.java
@@ -12,5 +12,18 @@ public class ArrayListTest {
     public static void main(String[] args) {
         List list = new ArrayList();
         list.add(1);
+        list.add(2);
+        list.add(3);
+        list.add(4);
+        list.add(5);
+        System.out.println(list.get(0));
+        System.out.println(list.get(4));
+        System.out.println(list.size());
+
+        System.out.println("==========");
+        String[] strings = new String[5];
+        for (int i = 0; i < strings.length; i++) {
+            System.out.println(strings[i]);
+        }
     }
 }
