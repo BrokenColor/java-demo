12a11bee304b1d379d7e112084e0fe227d67b59f
guwanli <guwanli@ushareit.com>
2021-12-17 20:42:53 +0800

    122. 买卖股票的最佳时机 II

---
+++ src/main/java/algorithmCode/InterestingCode/LC_122_M_MaxProfit.java
@@ -0,0 +1,25 @@
+package algorithmCode.InterestingCode;
+
+/**
+ * 122. 买卖股票的最佳时机 II
+ *
+ * @author guwanli
+ * @date 2021/12/17 10:24
+ */
+public class LC_122_M_MaxProfit {
+    public static void main(String[] args) {
+        LC_122_M_MaxProfit solution = new LC_122_M_MaxProfit();
+        int[] prices = {7, 1, 5, 3, 6, 4};
+        System.out.println(solution.maxProfit(prices));
+    }
+
+    public int maxProfit(int[] prices) {
+        int maxProfit = 0;
+        for (int i = 0; i < prices.length - 1; i++) {
+            if (prices[i] < prices[i + 1]) {
+                maxProfit += prices[i + 1] - prices[i];
+            }
+        }
+        return maxProfit;
+    }
+}
