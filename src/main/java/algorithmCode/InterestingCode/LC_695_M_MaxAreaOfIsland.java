714df8bc56207513fa8764def837ace27fdc2188
BrokenColor <huajianzhiyue@sina.com>
2022-01-11 11:24:22 +0800

    695. 岛屿的最大面积

---
+++ src/main/java/algorithmCode/InterestingCode/LC_695_M_MaxAreaOfIsland.java
@@ -0,0 +1,47 @@
+package algorithmCode.InterestingCode;
+
+/**
+ * 695. 岛屿的最大面积
+ *
+ * @date 2022/1/11 10:18
+ */
+public class LC_695_M_MaxAreaOfIsland {
+
+    public static void main(String[] args) {
+        LC_695_M_MaxAreaOfIsland solution = new LC_695_M_MaxAreaOfIsland();
+        int[][] grid = {
+                {0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0},
+                {0, 1, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0},
+                {0, 1, 0, 0, 1, 1, 0, 0, 1, 0, 1, 0, 0},
+                {0, 1, 0, 0, 1, 1, 0, 0, 1, 1, 1, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0}};
+        System.out.println(solution.maxAreaOfIsland(grid));
+    }
+
+    //1 (代表土地); 0（代表水）
+    public int maxAreaOfIsland(int[][] grid) {
+        if (grid.length < 1 || grid[0].length < 1) return 0;
+        int res = 0, m = grid.length, n = grid[0].length;
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (grid[i][j] == 1) {
+                    res = Math.max(res, dfs(grid, i, j));
+                }
+            }
+        }
+        return res;
+    }
+
+    private int dfs(int[][] grid, int i, int j) {
+        if (i < 0 || j < 0 || i >= grid.length || j >= grid[0].length) return 0;
+
+        if (grid[i][j] == 0) return 0;
+
+        grid[i][j] = 0;
+
+        return dfs(grid, i - 1, j) + dfs(grid, i + 1, j) + dfs(grid, i, j - 1) + dfs(grid, i, j + 1) + 1;
+    }
+}
