713ceca7b6db7155c90a3b2975beb8953af5d895
BrokenColor <huajianzhiyue@sina.com>
2022-01-12 11:01:08 +0800

    1905. 统计子岛屿

---
+++ src/main/java/algorithmCode/InterestingCode/LC_1905_M_CountSubIslands.java
@@ -0,0 +1,59 @@
+package algorithmCode.InterestingCode;
+
+/**
+ * 1905. 统计子岛屿
+ *
+ * @date 2022/1/12 10:16
+ */
+public class LC_1905_M_CountSubIslands {
+    public static void main(String[] args) {
+        LC_1905_M_CountSubIslands solution = new LC_1905_M_CountSubIslands();
+        int[][] grid1 = {
+                {1, 1, 1, 0, 0},
+                {0, 1, 1, 1, 1},
+                {0, 0, 0, 0, 0},
+                {1, 0, 0, 0, 0},
+                {1, 1, 0, 1, 1}};
+        int[][] grid2 = {
+                {1, 1, 1, 0, 0},
+                {0, 0, 1, 1, 1},
+                {0, 1, 0, 0, 0},
+                {1, 0, 1, 1, 0},
+                {0, 1, 0, 1, 0}};
+        System.out.println(solution.countSubIslands(grid1, grid2));
+    }
+
+    //0 （表示水域）和 1 （表示陆地）
+    public int countSubIslands(int[][] grid1, int[][] grid2) {
+        int res = 0, m = grid2.length, n = grid2[0].length;
+        //把不符合子岛的打沉
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (grid1[i][j] == 0 && grid2[i][j] == 1) {
+                    // 这个岛屿肯定不是子岛，淹掉
+                    dfs(grid2, i, j);
+                }
+            }
+        }
+        // 现在 grid2 中剩下的岛屿都是子岛，计算岛屿数量
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (grid2[i][j] == 1) {
+                    res++;
+                    dfs(grid2, i, j);
+                }
+            }
+        }
+        return res;
+    }
+
+    private void dfs(int[][] grid, int i, int j) {
+        if (i < 0 || j < 0 || i >= grid.length || j >= grid[0].length || grid[i][j] == 0) return;
+
+        grid[i][j] = 0;
+        dfs(grid, i - 1, j);
+        dfs(grid, i + 1, j);
+        dfs(grid, i, j - 1);
+        dfs(grid, i, j + 1);
+    }
+}
