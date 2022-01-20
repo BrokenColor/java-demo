27e79108f3e122004b726f7e876c0e5f55eeada7
BrokenColor <huajianzhiyue@sina.com>
2022-01-20 10:31:13 +0800

    219. 存在重复元素 II

---
+++ src/main/java/algorithmCode/InterestingCode/LC_219_S_ContainsNearbyDuplicate.java
@@ -0,0 +1,39 @@
+package algorithmCode.InterestingCode;
+
+/**
+ * 219. 存在重复元素 II
+ *
+ * @date 2022/1/19 10:24
+ */
+public class LC_219_S_ContainsNearbyDuplicate {
+    public static void main(String[] args) {
+        LC_219_S_ContainsNearbyDuplicate solution = new LC_219_S_ContainsNearbyDuplicate();
+//        int[] nums = {1, 2, 3, 1};
+//        int k = 3;
+//        int[] nums = {1, 2, 3, 1, 2, 3};
+        int[] nums = {99, 99};
+        int k = 2;
+        System.out.println(solution.containsNearbyDuplicate(nums, k));
+    }
+
+    /**
+     * 定义两个指针，p1 p2=p1+k;,判断两个指针中间是否有相等元素
+     * O(N*N)
+     *
+     * @param nums 数组
+     * @param k    区间
+     * @return res
+     */
+    public boolean containsNearbyDuplicate(int[] nums, int k) {
+        if (nums.length <= 1 && k == 0) return true;
+        boolean res = false;
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = i + 1; j < nums.length && j <= i + k; j++) {
+                if (nums[i] == nums[j]) {
+                    return true;
+                }
+            }
+        }
+        return res;
+    }
+}
