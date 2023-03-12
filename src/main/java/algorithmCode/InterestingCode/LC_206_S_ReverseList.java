e893f69da9214a4a450be0c78fb9507f8ffe0a94
guwanli <huajianzhiyue@sina.com>
2023-03-12 16:58:01 +0800

    206.反转链表

--- src/main/java/algorithmCode/InterestingCode/LC_206_S_ReverseList.java
+++ src/main/java/algorithmCode/InterestingCode/LC_206_S_ReverseList.java
@@ -12,9 +12,13 @@ public class LC_206_S_ReverseList {
     public ListNode reverseList(ListNode head) {
         ListNode pre = null, curr = head, next;
         while (curr != null) {
+            //存储下一个节点的指针，否则修改完节点指针就找不到下一个节点了
             next = curr.next;
+            //指针修改：把前置节点赋值给当前节点的next
             curr.next = pre;
+            //把处理完的节点存到pre节点上
             pre = curr;
+            //处理下一个节点
             curr = next;
         }
         return pre;
