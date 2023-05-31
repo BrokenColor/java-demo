8fe02061a6c2b41195a580d473c97b0334a26469
guwanl <guwanl@yonyou.com>
2023-05-31 19:32:34 +0800

    28. 找出字符串中第一个匹配项的下标

--- src/main/java/algorithmCode/InterestingCode/LC_28_M_strStr.java
+++ src/main/java/algorithmCode/InterestingCode/LC_28_M_strStr.java
@@ -30,4 +30,21 @@ public class LC_28_M_strStr {
         }
         return -1;
     }
+
+    public int strStr1(String haystack, String needle) {
+        int m = haystack.length(), n = needle.length();
+        //遍历haystack
+        for (int i = 0; i <= m - n; i++) {
+            // 从原串的「发起点」和匹配串的「首位」开始，尝试匹配
+            int a = i, b = 0;
+            while (b < n && haystack.charAt(a) == needle.charAt(b)) {
+                a++;
+                b++;
+            }
+            // 如果能够完全匹配，返回原串的「发起点」下标
+            if (n == b) return i;
+
+        }
+        return -1;
+    }
 }
