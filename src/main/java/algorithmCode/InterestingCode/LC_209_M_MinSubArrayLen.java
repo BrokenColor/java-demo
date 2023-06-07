bfb1952f095c1d2ef98a9612c40fb5e1dd15a977
guwanli <huajianzhiyue@sina.com>
2023-06-07 21:54:01 +0800

    209. 长度最小的子数组

---
+++ src/main/java/algorithmCode/InterestingCode/LC_209_M_MinSubArrayLen.java
@@ -0,0 +1,35 @@
+package algorithmCode.InterestingCode;
+
+/**
+ * 209. 长度最小的子数组
+ *
+ * @Author bc
+ * @Date 2023/6/7 21:05
+ * @Description: 滑动窗口
+ */
+public class LC_209_M_MinSubArrayLen {
+
+    public static void main(String[] args) {
+        int[] nums = {2, 3, 1, 2, 4, 3};
+//        int[] nums = {1, 4, 4};
+        LC_209_M_MinSubArrayLen solution = new LC_209_M_MinSubArrayLen();
+        System.out.println(solution.minSubArrayLen(7, nums));
+        System.out.println(solution.minSubArrayLen(4, nums));
+    }
+
+    public int minSubArrayLen(int target, int[] nums) {
+        int length = nums.length;
+        int reslut = Integer.MAX_VALUE, sum = 0;
+        int left = 0, right = 0;
+        while (right < length) {
+            sum += nums[right];
+            while (sum >= target) {
+                reslut = Math.min(reslut, right - left + 1);
+                sum -= nums[left];
+                left++;
+            }
+            right++;
+        }
+        return reslut == Integer.MAX_VALUE ? 0 : reslut;
+    }
+}
