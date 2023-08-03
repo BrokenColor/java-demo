90b40ed2d425949a5721e7a5e4ad42f3a11c7440
guwanli <huajianzhiyue@sina.com>
2023-08-03 20:36:15 +0800

    155.最小栈

---
+++ src/main/java/algorithmCode/InterestingCode/LC_155_M_MinStack.java
@@ -0,0 +1,53 @@
+package algorithmCode.InterestingCode;
+
+import java.util.Deque;
+import java.util.LinkedList;
+
+/**
+ * 155.最小栈
+ * @Author
+ * @Date 2023/8/3 20:33
+ * @Description: 最小栈
+ */
+public class LC_155_M_MinStack {
+    public static void main(String[] args) {
+        MinStack minStack = new MinStack();
+        minStack.push(-2);
+        minStack.push(0);
+        minStack.push(-3);
+        System.out.println(minStack.getMin());
+        minStack.pop();
+        System.out.println(minStack.top());
+        System.out.println(minStack.getMin());
+    }
+}
+
+class MinStack {
+
+    private Deque<Integer> stack;
+    private Deque<Integer> minStack;
+
+    public MinStack() {
+        stack = new LinkedList<>();
+        minStack = new LinkedList<>();
+        minStack.push(Integer.MAX_VALUE);
+    }
+
+    public void push(int val) {
+        stack.push(val);
+        minStack.push(Math.min(minStack.peek(),val));
+    }
+
+    public void pop() {
+        stack.pop();
+        minStack.pop();
+    }
+
+    public int top() {
+        return stack.peek();
+    }
+
+    public int getMin() {
+        return minStack.peek();
+    }
+}
