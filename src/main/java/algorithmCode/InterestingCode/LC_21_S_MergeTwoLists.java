6d37d90e1525a225bc487789237c400ade1406f5
guwanl <guwanl@yonyou.com>
2023-08-08 19:53:24 +0800

    21. 合并两个有序链表

--- src/main/java/algorithmCode/InterestingCode/LC_21_S_MergeTwoLists.java
+++ src/main/java/algorithmCode/InterestingCode/LC_21_S_MergeTwoLists.java
@@ -43,4 +43,21 @@ public class LC_21_S_MergeTwoLists {
         p.next = p1 == null ? p2 : p1;
         return listNode.next;
     }
+
+    /**
+     * 递归
+     */
+    public ListNode mergeTwoLists1(ListNode list1, ListNode list2) {
+        if (list1 == null) {
+            return list2;
+        } else if (list2 == null) {
+            return list1;
+        } else if (list1.val < list2.val) {
+            list1.next = mergeTwoLists1(list1.next, list2);
+            return list1;
+        } else {
+            list2.next = mergeTwoLists1(list1, list2.next);
+            return list2;
+        }
+    }
 }
