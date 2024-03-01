8da9bcbc3f9322c3d219d519dff152be8db611f8
guwanli <huajianzhiyue@sina.com>
2024-03-01 12:22:34 +0800

    322 零钱兑换

--- src/main/java/algorithmCode/InterestingCode/LC_322_M_CoinChange.java
+++ src/main/java/algorithmCode/InterestingCode/LC_322_M_CoinChange.java
@@ -46,4 +46,19 @@ public class LC_322_M_CoinChange {
         memo[amount] = res == Integer.MAX_VALUE ? -1 : res;
         return memo[amount];
     }
+
+    public static int coinChange(int[] coins, int amount) {
+        int[] dp = new int[amount];
+        Arrays.fill(dp, amount + 1);
+        dp[0] = 0;
+        for (int i = 0; i < dp.length; i++) {
+            for (int coin : coins) {
+                if (i - coin < 0) {
+                    continue;
+                }
+                dp[i] = Math.min(dp[i], dp[i - coin] + 1);
+            }
+        }
+        return (dp[amount] == amount + 1) ? -1 : dp[amount];
+    }
 }
